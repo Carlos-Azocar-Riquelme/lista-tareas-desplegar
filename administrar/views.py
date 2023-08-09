9ad9cebdd50771b78bb67e1ea7e077f65eec0f0e
cazocar <cazocar@utalca.cl>
2023-08-08 23:27:43 -0400

    arreglando bug

--- administrar/views.py
+++ administrar/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from administrar.models import Tarea  # Importar el modelo
-from .forms import TareaForm  #Para validar
-
+from .forms import TareaForm  # Para validar
 
 # Create your views here.
+
+
 def v_index(request):
     if request.method == 'POST':
         ######
@@ -39,17 +40,17 @@ def v_index(request):
         consulta = Tarea.objects.filter(
             titulo__icontains=request.GET.get("titulo", ""))
 
-        #https://lista-tareas.lokcito.repl.co/?estado=1  Filtra a los completados
-        #https://lista-tareas.lokcito.repl.co/?estado=0  Filtra a los pendientes
-        #https://lista-tareas.lokcito.repl.co/?estado=10000  Filtra a ninguno
-        #https://lista-tareas.lokcito.repl.co/?estado=  No Filtra
-        #https://lista-tareas.lokcito.repl.co/  No filtra
-        #https://lista-tareas.lokcito.repl.co/?titulo=Bailar No filtra
+        # https://lista-tareas.lokcito.repl.co/?estado=1  Filtra a los completados
+        # https://lista-tareas.lokcito.repl.co/?estado=0  Filtra a los pendientes
+        # https://lista-tareas.lokcito.repl.co/?estado=10000  Filtra a ninguno
+        # https://lista-tareas.lokcito.repl.co/?estado=  No Filtra
+        # https://lista-tareas.lokcito.repl.co/  No filtra
+        # https://lista-tareas.lokcito.repl.co/?titulo=Bailar No filtra
 
         if request.GET.get("estado", "") != "":
             consulta = consulta.filter(estado=request.GET.get("estado", ""))
 
-        ## Listar las tareas
+        # Listar las tareas
         context = {
             'var1': 'Valor1',
             'var2': 'Valorasdasdasdasdasd',
@@ -71,21 +72,19 @@ def v_completado(request, tarea_id):
 
 
 def v_login(request):
-    from .forms import LoginForm  #Importanto formulario
+    from .forms import LoginForm  # Importanto formulario
     from django.contrib.auth import authenticate, login
-
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():  # Verifica que los datos necesarios
 
             user = authenticate(username=form.cleaned_data["username"],
-                                password=form.cleaned_data["password"]
-                                )  # Comprueba que la contrasena es valida
+                                password=form.cleaned_data["password"])  # Comprueba que la contrasena es valida
 
             if user is not None:  # Usuario y contrasena bien
-                login(user)
+                login(request, user)
                 return HttpResponseRedirect("/")  # Redirigir
-            else:  #Usuario y contrasena erroneos
+            else:  # Usuario y contrasena erroneos
                 return HttpResponseRedirect("/")  # Redirigir
         else:
             # Los datos no son correctos
